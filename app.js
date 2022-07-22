0fede58ad97cd40fa36da7ba8c1c787fb4e9df90
Ishpaul Singh <ishpaul207@gmail.com>
2022-07-22 15:05:51 +0530

    integrated api

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const adviceEl = document.querySelector('#advice')
 const adviceNumEl = document.querySelector('#advice-num')
-const diceContainer = document.querySelector('.dice-container')
+const jokeEl = document.querySelector('#joke')
+const jokeNumEl = document.querySelector('#joke-num')
+const adviceDiceContainer = document.querySelector('.advice-dice')
+const jokeDiceContainer = document.querySelector('.joke-dice')
 
 async function getAdvice(){
     const response = await fetch('https://api.adviceslip.com/advice')
@@ -9,6 +12,46 @@ async function getAdvice(){
     adviceNumEl.textContent =  aboutAdvice.slip.id
 }
 
+async function fetchJoke() {
+    const response = await fetch("http://icanhazdadjoke.com", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const jokeData = await response.json();
+    jokeEl.textContent = jokeData.joke
+    jokeNumEl.textContent =  jokeData.id
+  }
 
-diceContainer.addEventListener('click', getAdvice)
-document.addEventListener('DOMContentLoaded', getAdvice)
+
+adviceDiceContainer.addEventListener('click', (e)=> {
+    e.preventDefault()
+    adviceEl.textContent = 'loading..'
+    adviceNumEl.textContent =  'loading...'
+    setTimeout(() => {
+        getAdvice()
+    }, 700); 
+})
+jokeDiceContainer.addEventListener('click', (e)=> {
+    e.preventDefault()
+    jokeEl.textContent = 'loading..'
+    jokeNumEl.textContent =  'loading...'
+    setTimeout(() => {
+        fetchJoke()
+    }, 700); 
+});
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    adviceEl.textContent = 'loading..'
+    adviceNumEl.textContent =  'loading...'
+    jokeEl.textContent = 'loading..'
+    jokeNumEl.textContent =  'loading...'
+    setTimeout(() => {
+        getAdvice()
+    }, 1000); 
+    setTimeout(() => {
+        fetchJoke()
+    }, 1000); 
+    getAdvice();
+    fetchJoke();
+});
